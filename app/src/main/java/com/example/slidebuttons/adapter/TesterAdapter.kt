1ca1e6535d51a868e74e223bc9b735f44af00e6f
aiman <aimanxkiller@gmail.com>
2023-09-29 00:05:46 +0800

    Haha

--- app/src/main/java/com/example/slidebuttons/adapter/TesterAdapter.kt
+++ app/src/main/java/com/example/slidebuttons/adapter/TesterAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.daimajia.swipe.SimpleSwipeListener
 import com.daimajia.swipe.SwipeLayout
 import com.example.slidebuttons.ListClickListener
 import com.example.slidebuttons.People
@@ -16,7 +18,6 @@ import com.example.slidebuttons.activity.FavLayoutActivity
 class TesterAdapter(private val context: FavLayoutActivity, private var list: List<People>) :RecyclerView.Adapter<TesterAdapter.ViewHolder>() {
 
     private var listener: ListClickListener? = null
-
     fun setItemClickListener(listener: ListClickListener) {
         this.listener = listener
     }
@@ -27,6 +28,7 @@ class TesterAdapter(private val context: FavLayoutActivity, private var list: Li
         val acc:TextView = itemView.findViewById(R.id.tv_account_no)
         val bankName:TextView = itemView.findViewById(R.id.tv_bank_name)
         val divider:ImageView = itemView.findViewById(R.id.item_list_divider)
+        val view:RelativeLayout = itemView.findViewById(R.id.surface_view)
 
         val btn:ImageView = itemView.findViewById(R.id.btn_3dots)
         val h_btn1:ImageView = itemView.findViewById(R.id.hdn_btn_1)
@@ -46,6 +48,8 @@ class TesterAdapter(private val context: FavLayoutActivity, private var list: Li
         return ViewHolder(view)
     }
 
+    private var currentlyOpenSwipeLayout: SwipeLayout? = null
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = list[position]
         holder.name.text = data.name
@@ -58,13 +62,33 @@ class TesterAdapter(private val context: FavLayoutActivity, private var list: Li
             holder.pin.visibility = View.GONE
         }
 
+        //Start
         holder.btn.setOnClickListener {
+            if (currentlyOpenSwipeLayout != null && currentlyOpenSwipeLayout != holder.swiper) {
+                currentlyOpenSwipeLayout?.close()
+            }
             if (holder.swiper.openStatus == SwipeLayout.Status.Open){
                 holder.swiper.close()
             }else if(holder.swiper.openStatus == SwipeLayout.Status.Close){
                 holder.swiper.open()
             }
+            currentlyOpenSwipeLayout = holder.swiper
         }
+        // Close the currently open SwipeLayout when user starts dragging another one
+        holder.swiper.addSwipeListener(object : SimpleSwipeListener() {
+            override fun onStartOpen(layout: SwipeLayout) {
+                if (currentlyOpenSwipeLayout != null && currentlyOpenSwipeLayout != layout) {
+                    currentlyOpenSwipeLayout?.close()
+                }
+            }
+        })
+        // Set the currently open SwipeLayout when it's opened or closed
+        holder.swiper.addSwipeListener(object : SimpleSwipeListener() {
+            override fun onOpen(layout: SwipeLayout) {
+                currentlyOpenSwipeLayout = layout
+            }
+        })
+        //End
 
         holder.h_btn1.setOnClickListener {
             listener?.onItemPin(data.id)
@@ -81,8 +105,10 @@ class TesterAdapter(private val context: FavLayoutActivity, private var list: Li
         }
     }
 
+
     override fun getItemCount(): Int {
         return list.size
     }
+
 
 }
